227e1ea2587570bbf17136bc328c4c49f1dcfd0b
Rarizz <abyan.rahmanalfariz06@gmail.com>
2023-11-07 13:42:09 +0700

    #15 Mengubah Data

--- controllers/users.js
+++ controllers/users.js
@@ -37,24 +37,33 @@ module.exports = {
         }
 
       },
-      update: (req, res) => {
+      update: async (req, res) => {
+        try {
+            const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+                new: true,
+                runValidators: true
+            })
+            res.json({
+                status: true, 
+                data: user,
+                method: req.method, 
+                url: req.url,
+                message: "Data berhasil diubah"
+            })
+
+        } catch (error) {
+            res.status(400).json({success: false})
+        }
         const id = req.params.id
-        users.filter(user => {
-            if (user.id == id){
-                user.nama = req.body.nama
-                user.email = req.body.email
-                return user
-            }
-        })
-        res.json({
-            status: true, 
-            data: users,
-            method: req.method, 
-            url: req.url,
-            message: "Data berhasil diubah"
-        })
+        
+        
       },
-      delete: (req, res) =>{
+      delete: async (req, res) =>{
+        try {
+            
+        } catch (error) {
+            
+        }
         const id = req.params.id
         users = users.filter(user => user.id != id)
     
